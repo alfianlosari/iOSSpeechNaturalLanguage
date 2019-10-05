06cb5dc682c45f4266682ce2e3a5d51e3a638697
Alfian Losari <alfianlosari@gmail.com>
2019-10-06 01:52:11 +0700

    Initial Commit

---
+++ SpeecherLang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeecherLang
+//
+//  Created by Alfian Losari on 06/10/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
